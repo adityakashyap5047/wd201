d5724fa77561dd989bbb2140a5316bbb7602db48
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-09 03:48:20 +0530

    deleting a file using node.js

--- http-server/index.js
+++ http-server/index.js
@@ -18,12 +18,19 @@ fs.readFile("sample.txt", (err, data) => {
 
 //appendFile() method is used to append data to a file. It creates a new file if the specified file does not exist.
 fs.appendFile("sample.txt", " This is my updated content", (err) => {
-    if (err) throw err;
-    console.log("File updated!");
-  });
+  if (err) throw err;
+  console.log("File updated!");
+});
 
 //rename() method renames the specified file.
-  fs.rename("sample.txt", "test.txt", (err) => {
-    if (err) throw err;
-    console.log("File name updated!");
-  });
+fs.rename("sample.txt", "test.txt", (err) => {
+  if (err) throw err;
+  console.log("File name updated!");
+});
+
+//unlink() method deletes the specified file.
+fs.unlink("test.txt", (err) => {
+  if (err) throw err;
+  console.log("File test.txt deleted successfully!");
+});
+
