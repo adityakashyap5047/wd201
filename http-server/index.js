4db159e29ecb5f2c621d87bcc3982adbadb785dd
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-09 03:45:02 +0530

    append the content

--- http-server/index.js
+++ http-server/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+//writeFile() method is used to write data to a file. It replaces the file if it already exists.
 fs.writeFile(
   "sample.txt",
   "Hello World. Welcome to Node.js File System module.",
@@ -9,7 +10,14 @@ fs.writeFile(
   }
 );
 
+//readFile() method is used to read data from a file. It returns the contents in the form of a buffer.
 fs.readFile("sample.txt", (err, data) => {
   if (err) throw err;
   console.log(data.toString());
 });
+
+//appendFile() method is used to append data to a file. It creates a new file if the specified file does not exist.
+fs.appendFile("sample.txt", " This is my updated content", (err) => {
+    if (err) throw err;
+    console.log("File updated!");
+  });
