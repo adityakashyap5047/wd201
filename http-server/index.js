a280e7e6e85cad513d94b177277c7853a4f81a9a
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-09 03:43:16 +0530

    reading a file with node.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,10 +1,15 @@
 const fs = require("fs");
 
 fs.writeFile(
-    "sample.txt",
-    "Hello World. Welcome to Node.js File System module.",
-    (err) => {
-      if (err) throw err;
-      console.log("File created!");
-    }
-  );
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+
+fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
