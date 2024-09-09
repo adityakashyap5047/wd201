7fa0ccb5cfe4093eff9176fe2a25c4d2edeb5f8b
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-09 17:22:34 +0530

    creating the route using http

--- http-server/index.js
+++ http-server/index.js
@@ -1,16 +1,32 @@
 const http = require("http");
 const fs = require("fs");
 
-
+let homeContent = "";
+let projectContent = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
     console.log(err);
   } 
-  http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(home);
-    res.end();
-  })
-  .listen(3000);
+  homeContent = home;
 });
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    console.log(err);
+  } 
+  projectContent = project;
+});
+
+http.createServer((req, res) => {
+  let url = req.url;
+  res.writeHead(200, { "Content-Type": "text/html" });
+  switch (url) {
+    case "/project":
+      res.write(projectContent);
+      break;
+    default:
+      res.write(homeContent);
+      break;
+  }
+}).listen(8080);
