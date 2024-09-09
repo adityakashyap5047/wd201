f730053b927392fe200fecb3b54aca1e0611630a
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-09 16:52:57 +0530

    starting the server for the home page uisng http

--- http-server/index.js
+++ http-server/index.js
@@ -1,10 +1,16 @@
 const http = require("http");
 const fs = require("fs");
 
+
+
 fs.readFile("home.html", (err, home) => {
   if (err) {
     console.log(err);
-  } else {
-    console.log(home.toString());
-  }
+  } 
+  http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(home);
+    res.end();
+  })
+  .listen(3000);
 });
