c04238215ba1f792b9bec9f6d027710ac85030a1
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-12 16:03:14 +0530

    creating the migration and model generation

--- todo-cli/eslint.config.mjs
+++ todo-cli/eslint.config.mjs
@@ -2,7 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
+  {
+    ignores: ["models/", "migrations/"], // Add the folders you want to ignore here
+  },
 ];
