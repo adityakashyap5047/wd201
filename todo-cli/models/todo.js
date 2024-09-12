0cfbdd1fe614d0280bc14971a7c608a0be5bcefd
Aditya Kashyap <adityakashyap5047@gmail.com>
2024-09-12 16:23:18 +0530

    Modified the test

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -7,8 +7,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    static async addTask(params) {
+      return await Todo.create(params);
+    }
     static associate(models) {
       // define association here
+    }
+    displayableString() {
+      let checkbox = this.completed ? "[x]" : "[ ]";
+      return `${this.id}. ${checkbox} ${this.title} ${this.dueDate}`;
     }
   }
   Todo.init(
